aa65ac1006e0cbab35cf26fab9100290a4cc5ad3
v1v3k <vivekn936@gmail.com>
2023-08-18 18:51:07 +0545

    Contact Page Layout Improved

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import ContactImg from "../../../public/contact.png"
 
 const Contact = () => {
   return (
-    <div className="flex flex-col justify-center">
-      <h1 className="font-bold text-4xl mb-16 mx-auto">Let&apos;s Keep in Touch</h1>
-      <div className="flex flex-col md:flex-row gap-28 basis-1/2">
-        <div className="img-container">
-          <Image width="500" height="500" src="/contact.png" alt="Contact image" />
+    <div className="mb-5">
+      <h1 className="font-bold text-2xl md:text-4xl mb-5 text-center">Let&apos;s Keep in Touch</h1>
+      <div className="flex flex-col md:flex-row justify-around items-center gap-5">
+        <div className="img-container relative w-[300px] h-[300px] sm:w-[450px] sm:h-[450px] md:w-[500px] md:h-[500px]">
+          <Image fill={true} src={ContactImg} alt="Contact image" className="object-cover"/>
         </div>
-        <div className="form flex flex-col space-y-4 basis-1/2">
+        <div className="form flex flex-col space-y-4 basis-full md:basis-1/2">
           <input className="p-2 rounded-md" type="text" placeholder="Enter Your Name" />
           <input className="p-2 rounded-md" type="email" placeholder="Enter Your Email" />
           <textarea className="p-2 rounded-md" name="message" cols="30" rows="10" placeholder="Enter Your Message"></textarea>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
+import {FaFacebookSquare, FaInstagram, FaLinkedin, FaTwitterSquare, FaYoutubeSquare,FaWhatsapp} from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div className='flex gap-2 md:justify-between md:fixed bottom-2 w-[84%] flex-col items-center md:flex-row'>
-        <div className='text-xs'>©2023 Digital Nepal. All rights reserved.</div>
+    <div className='flex gap-2 md:justify-between md:fixed bottom-2 md:w-[84%] flex-col items-center md:flex-row'>
+        <div className='text-sm'>©2023 Digital Nepal. All rights reserved.</div>
         <div className="flex gap-4 cursor-pointer">
-            <Image src="./1.png" alt='facebook-icon' height={15} width={18}></Image>
-            <Image src="./2.png" alt='insta-icon' height={15} width={18}></Image>
-            <Image src="./3.png" alt='twitter-icon' height={15} width={18}></Image>
-            <Image src="./4.png" alt='youtube-icon' height={15} width={18}></Image>
+            <FaFacebookSquare size={20}/>
+            <FaInstagram size={20}/>
+            <FaLinkedin size={20}/>
+            <FaTwitterSquare size={20}/>
+            <FaYoutubeSquare size={20}/>
+            <FaWhatsapp size={21}/>
         </div>
     </div>
   )
