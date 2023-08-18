d69bb651ca7d1fed769265f91a3ae651cc8319b1
v1v3k <vivekn936@gmail.com>
2023-08-18 20:25:46 +0545

    NavBar Made Responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 // import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import Link from 'next/link';
+import {CgMenu} from "react-icons/cg"
+import {AiFillCloseCircle} from "react-icons/ai"
 
 const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const openMenu = ()=>{
+        toggleMenu();
+        const sideMenu = document.getElementById("sidemenu");
+        sideMenu.style.right="0px";
+        sideMenu.style.width="200px"
+        sideMenu.style.padding="40px 20px"
+        sideMenu.style.height="100%"
+    }
+
+    const closeMenu = ()=> {
+        toggleMenu();
+        const sideMenu = document.getElementById("sidemenu");
+        sideMenu.style.right="-250px";
+        sideMenu.style.width="fit-content"
+        sideMenu.style.height="fit-content"
+    }
+
+    const toggleMenu = ()=>{
+        setIsMenuOpen(!isMenuOpen);
+    }
     const links = [
         {
             id: 1,
@@ -40,16 +66,19 @@ const Navbar = () => {
             url: "/dashboard"
         },
     ]
+
   return (
     <div className="flex justify-between h-24 items-center">
         <div className='cursor-pointer font-bold text-2xl'>
             <Link href="/">Digital Nepal</Link> 
         </div>
-        <div className="flex gap-5 flex-col md:flex-row">
+        <CgMenu size={20} className='md:hidden cursor-pointer' onClick={()=>openMenu()}/>
+        <div className={`flex gap-5 flex-col md:flex-row fixed md:static top-0 -right-[350px] z-10 transition-all bg-gray-900 md:bg-transparent`} id='sidemenu'>
             {links.map((link)=>(
             <Link href={link.url} key={link.id}>{link.title}</Link>
             ))}
             <button className='bg-green-600 px-2 rounded-full'>Logout</button>
+            <AiFillCloseCircle size={25} className={`md:hidden cursor-pointer fixed top-10 right-5 ${isMenuOpen? "block":"hidden"}`} onClick={()=>closeMenu()}/>
         </div>
     </div>
   )
