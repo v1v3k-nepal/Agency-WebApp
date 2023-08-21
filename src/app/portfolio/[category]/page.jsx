e2da6b17ddfb077ab4f98047e389404ab1b87bb3
v1v3k <vivekn936@gmail.com>
2023-08-21 17:47:11 +0545

    Test: Dynamic Route v2

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import Button from '@/components/button/Button';
-import {items} from "./data.js"
+import {items} from "./data"
 import {notFound} from "next/navigation"
 
 const Category = ({params}) => {
