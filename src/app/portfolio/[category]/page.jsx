b2838bc60bed889b3743fc32f9139a9047d28926
v1v3k <vivekn936@gmail.com>
2023-08-23 18:24:05 +0545

    Test : Dynamic Route

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image';
 import Button from '@/components/button/Button';
 // import {Collection} from "./data"
-// import {notFound} from "next/navigation"
+import {notFound} from "next/navigation"
 
 const Collection = {
   applications: [
