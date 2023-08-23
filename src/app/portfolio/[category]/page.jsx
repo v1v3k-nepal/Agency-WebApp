5bfc58deb6518e2e37a683f4608484a6040f7644
v1v3k <vivekn936@gmail.com>
2023-08-23 17:55:37 +0545

    Testing: Dynamic Route at Production

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -7,7 +7,7 @@ import {notFound} from "next/navigation"
 const getData = (cat)=>{
   const catData = dataCollection[cat];
   if(catData) return catData
-  else return notFound();
+  // else return notFound();
 }
 
 const Category = ({params}) => {
@@ -21,7 +21,7 @@ const Category = ({params}) => {
               <div key={item.id} className='flex gap-5 items-center flex-col-reverse md:flex-row md:odd:flex-row-reverse md:pb-10'>
               <div className={`left basis-1/2 ${(item.id)%2 !== 0? "mr-auto": ""}`}>
                 <h1 className='text-xl md:text-3xl font-bold mb-5 text-center md:text-left'>{item.title}</h1>
-                <p className='mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos deleniti velit obcaecati recusandae, laudantium culpa assumenda ipsum molestias numquam reiciendis maxime quis repellat maiores, magnam repudiandae, voluptatum ab dolorum harum consectetur illo labore optio repellendus quia iusto? Nisi, quas. Recusandae qui dignissimos minima aspernatur ut, maxime iste magni? Ea, rerum.</p>
+                <p className='mb-5'>{item.desc}</p>
                 <div className='mb-6'>
                 <Button url="/" text="See More"/>
                 </div>
