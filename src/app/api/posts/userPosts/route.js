cb8bb05ed9ffd3a67bfe39f1baf5fd915e196101
v1v3k <vivekn936@gmail.com>
2023-08-28 14:15:17 +0545

    Post Edit Feature Added

--- src/app/api/posts/userPosts/route.js
+++ src/app/api/posts/userPosts/route.js
@@ -27,3 +27,20 @@ export const POST = async (request) => {
     return new NextResponse("Failed to create new post", { status: 500 });
   }
 };
+
+
+export const PUT = async (request)=>{
+  const body = await request.json();
+  const postId = body._id;
+  try{
+    await connect();
+    const updatedPost = await Post.findByIdAndUpdate(postId, {$set: body}, {new: true})
+    if(updatedPost){
+      return new NextResponse("Post Has been updated successfully")
+    } else {
+      return new NextResponse("Post not found", { status: 404 });
+    }
+  }catch(err){
+    throw new Error(err)
+  }
+}
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,42 +4,49 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import React from "react";
 import useSWR from "swr";
+import { useState } from "react";
 
 const Dashboard = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
+  const [isEditing, setIsEditing] = useState(false);
+  const [postId, setPostId] = useState("")
+
+  const initialFormData = {
+    title: "",
+    desc: "",
+    img: "",
+    content: "",
+    userpic: "",
+    // _id: ""
+  };
+
+  const [formdata, setFormdata] = useState(initialFormData);
+
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading, mutate } = useSWR(`/api/posts/userPosts?username=${session?.user.name}`, fetcher);
-  console.log(data);
+  // console.log(data);
 
   if (status === "loading") return <p>Loading</p>;
   if (status === "unauthenticated") router?.push("/dashboard/login");
 
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target[0].value;
-    const desc = e.target[1].value;
-    const img = e.target[2].value;
-    const authorImg = e.target[3].value;
-    const content = e.target[4].value;
-    try{
-      await fetch("/api/posts/userPosts",{
+    try {
+      await fetch("/api/posts/userPosts", {
         method: "POST",
         body: JSON.stringify({
-          title, 
-          desc, 
-          img, 
-          content, 
-          userpic: authorImg,
-          username : session?.user.name})
-      })
+          ...formdata,
+          username: session?.user.name,
+        }),
+      });
       mutate();
-      e.target.reset();
-    }catch(err){
+      setFormdata(initialFormData);
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   const handleDelete = async (id) => {
     try {
@@ -51,6 +58,44 @@ const Dashboard = () => {
       console.log(err);
     }
   };
+
+  const handleInputChange = (e)=>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormdata((prevData)=>({...prevData, [name]:value}));
+  }
+
+  const handleEdit = (post) => {
+    setIsEditing(true);
+    setFormdata({
+      title: post.title, 
+      desc: post.desc, 
+      img: post.img, 
+      userpic: post.userpic, 
+      content: post.content,
+    })
+    setPostId(post._id)
+  };
+
+  const handleUpdate = async (e)=>{
+    e.preventDefault();
+    try { 
+      await fetch("/api/posts/userPosts", {
+        method: "PUT",
+        body: JSON.stringify({
+          ...formdata,
+          _id: postId,
+          username: session?.user.name,
+        }),
+      });
+      mutate();
+      setFormdata(initialFormData);
+      setIsEditing(false)
+    } catch (err) {
+      console.log(err);
+    }
+
+  }
 
   if (status == "authenticated") {
     return (
@@ -66,11 +111,18 @@ const Dashboard = () => {
                 <div className="md:pr-8 max-w-[80vw] lg:max-w-[20vw]">
                   <h1 className="font-bold text-2xl mb-2 whitespace-nowrap overflow-hidden text-ellipsis">{post.title}</h1>
                   <p className="max-h-[50px] overflow-hidden text-ellipsis">{post.desc}</p>
-                  <button
-                    onClick={() => handleDelete(post._id)}
-                    className="py-2 px-4 bg-red-600 cursor-pointer rounded-md border-none w-fit text-white font-semibold mt-5">
-                    Delete
-                  </button>
+                  <div className="flex gap-5">
+                    <button
+                      onClick={() => handleEdit(post)}
+                      className="py-2 px-4 bg-green-600 cursor-pointer rounded-md border-none w-fit text-white font-semibold mt-5">
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(post._id)}
+                      className="py-2 px-4 bg-red-600 cursor-pointer rounded-md border-none w-fit text-white font-semibold mt-5">
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             ))
@@ -80,21 +132,46 @@ const Dashboard = () => {
         </div>
         <div className="lg:ml-auto lg:basis-[45%] mb-8">
           <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
-            <h1 className="mx-auto font-bold text-2xl">Add New Post</h1>
-            <input type="text" placeholder="Title" className="p-2 rounded-md text-black bg-[#eeeaf3]" />
-            <input type="text" placeholder="Description" className="p-2 rounded-md text-black bg-[#eeeaf3]" />
-            <input type="text" placeholder="Blog Image Url" className="p-2 rounded-md text-black bg-[#eeeaf3]" />
-            <input type="text" placeholder="Author Image Url" className="p-2 rounded-md text-black bg-[#eeeaf3]" />
+            <h1 className="mx-auto font-bold text-2xl">{isEditing? "Edit Your Post" : "Add New Post"}</h1>
+
+            <input type="text" placeholder="Title" 
+            name="title"
+            value={formdata.title} 
+            onChange={handleInputChange}
+            className="p-2 rounded-md text-black bg-[#eeeaf3]" />
+
+            <input type="text" placeholder="Description" 
+            name="desc"
+            value={formdata.desc} 
+            onChange={handleInputChange}
+            className="p-2 rounded-md text-black bg-[#eeeaf3]" />
+
+            <input type="text" placeholder="Blog Image Url" 
+            name="img"
+            value={formdata.img} 
+            onChange={handleInputChange}
+            className="p-2 rounded-md text-black bg-[#eeeaf3]" />
+
+            <input type="text" placeholder="Author Image Url" 
+            name="userpic"
+            value={formdata.userpic} 
+            onChange={handleInputChange}
+            className="p-2 rounded-md text-black bg-[#eeeaf3]" />
+
             <textarea
-              name="content"
               id="content"
               cols="30"
               rows="8"
               placeholder="Blog Content"
+              name="content"
+              value={formdata.content}
+              onChange={handleInputChange}
               className="rounded-md p-2 text-black bg-[#eeeaf3]"></textarea>
-            <button className="px-5 py-3 bg-[#6d4bd1] cursor-pointer rounded-md border-none w-full text-white font-semibold">
-              Submit
-            </button>
+              {isEditing ? (<button className="px-5 py-3 bg-[#6d4bd1] cursor-pointer rounded-md border-none w-full text-white font-semibold" onClick={(e)=>handleUpdate(e)}>
+              Update Post
+            </button>): (<button className="px-5 py-3 bg-[#6d4bd1] cursor-pointer rounded-md border-none w-full text-white font-semibold" type="submit">
+              Create Post
+            </button>)}
           </form>
         </div>
       </div>
